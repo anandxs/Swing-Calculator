ae3b6f253380c7712a0be11ff69e7f730b591dd0
Anand S <anandsatheeshk@gmail.com>
2023-03-25 21:12:50 +0530

    Set all button foreground color to BLACK and background color to WHITE

--- src/Calculator.java
+++ src/Calculator.java
@@ -38,95 +38,131 @@ public class Calculator implements ActionListener {
 		clearBtn.setBounds(20, 110, 125, 50);
 		clearBtn.addActionListener(this);
 		jf.add(clearBtn);
+		clearBtn.setForeground(Color.BLACK);
+		clearBtn.setBackground(Color.WHITE);
 		
-		delBtn = new JButton("Del");
+		delBtn = new JButton("<-");
 		delBtn.setBounds(170, 110, 50, 50);
 		delBtn.addActionListener(this);
 		jf.add(delBtn);
+		delBtn.setForeground(Color.BLACK);
+		delBtn.setBackground(Color.WHITE);
 		
 		divBtn = new JButton("/");
 		divBtn.setBounds(245, 110, 50, 50);
 		divBtn.addActionListener(this);
 		jf.add(divBtn);
+		divBtn.setForeground(Color.BLACK);
+		divBtn.setBackground(Color.WHITE);
 		
 		// row 2
 		sevenBtn = new JButton("7");
 		sevenBtn.setBounds(20, 185, 50, 50);
 		sevenBtn.addActionListener(this);
 		jf.add(sevenBtn);
+		sevenBtn.setForeground(Color.BLACK);
+		sevenBtn.setBackground(Color.WHITE);
 		
 		eightBtn = new JButton("8");
 		eightBtn.setBounds(95, 185, 50, 50);
 		eightBtn.addActionListener(this);
 		jf.add(eightBtn);
+		eightBtn.setForeground(Color.BLACK);
+		eightBtn.setBackground(Color.WHITE);
 		
 		nineBtn = new JButton("9");
 		nineBtn.setBounds(170, 185, 50, 50);
 		nineBtn.addActionListener(this);
 		jf.add(nineBtn);
+		nineBtn.setForeground(Color.BLACK);
+		nineBtn.setBackground(Color.WHITE);
 		
 		prodBtn = new JButton("*");
 		prodBtn.setBounds(245, 185, 50, 50);
 		prodBtn.addActionListener(this);
 		jf.add(prodBtn);
+		prodBtn.setForeground(Color.BLACK);
+		prodBtn.setBackground(Color.WHITE);
 		
 		// row 3
 		fourBtn = new JButton("4");
 		fourBtn.setBounds(20, 260, 50, 50);
 		fourBtn.addActionListener(this);
 		jf.add(fourBtn);
+		fourBtn.setForeground(Color.BLACK);
+		fourBtn.setBackground(Color.WHITE);
 		
 		fiveBtn = new JButton("5");
 		fiveBtn.setBounds(95, 260, 50, 50);
 		fiveBtn.addActionListener(this);
 		jf.add(fiveBtn);
+		fiveBtn.setForeground(Color.BLACK);
+		fiveBtn.setBackground(Color.WHITE);
 		
 		sixBtn = new JButton("6");
 		sixBtn.setBounds(170, 260, 50, 50);
 		sixBtn.addActionListener(this);
 		jf.add(sixBtn);
+		sixBtn.setForeground(Color.BLACK);
+		sixBtn.setBackground(Color.WHITE);
 		
 		diffBtn = new JButton("-");
 		diffBtn.setBounds(245, 260, 50, 50);
 		diffBtn.addActionListener(this);
 		jf.add(diffBtn);
+		diffBtn.setForeground(Color.BLACK);
+		diffBtn.setBackground(Color.WHITE);
 		
 		// row 4
 		oneBtn = new JButton("1");
 		oneBtn.setBounds(20, 335, 50, 50);
 		oneBtn.addActionListener(this);
 		jf.add(oneBtn);
+		oneBtn.setForeground(Color.BLACK);
+		oneBtn.setBackground(Color.WHITE);
 		
 		twoBtn = new JButton("2");
 		twoBtn.setBounds(95, 335, 50, 50);
 		twoBtn.addActionListener(this);
 		jf.add(twoBtn);
+		twoBtn.setForeground(Color.BLACK);
+		twoBtn.setBackground(Color.WHITE);
 		
 		threeBtn = new JButton("3");
 		threeBtn.setBounds(170, 335, 50, 50);
 		threeBtn.addActionListener(this);
 		jf.add(threeBtn);
+		threeBtn.setForeground(Color.BLACK);
+		threeBtn.setBackground(Color.WHITE);
 		
 		sumBtn = new JButton("+");
 		sumBtn.setBounds(245, 335, 50, 50);
 		sumBtn.addActionListener(this);
 		jf.add(sumBtn);
+		sumBtn.setForeground(Color.BLACK);
+		sumBtn.setBackground(Color.WHITE);
 		
 		// row 5
 		dotBtn = new JButton(".");
 		dotBtn.setBounds(20, 410, 50, 50);
 		dotBtn.addActionListener(this);
 		jf.add(dotBtn);
+		dotBtn.setForeground(Color.BLACK);
+		dotBtn.setBackground(Color.WHITE);
 		
 		zeroBtn = new JButton("0");
 		zeroBtn.setBounds(95, 410, 50, 50);
 		zeroBtn.addActionListener(this);
 		jf.add(zeroBtn);
+		zeroBtn.setForeground(Color.BLACK);
+		zeroBtn.setBackground(Color.WHITE);
 		
 		equalBtn = new JButton("=");
 		equalBtn.setBounds(170, 410, 125, 50);
 		equalBtn.addActionListener(this);
 		jf.add(equalBtn);
+		equalBtn.setForeground(Color.BLACK);
+		equalBtn.setBackground(Color.WHITE);
 		
 		jf.setVisible(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
